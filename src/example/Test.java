944216a26349f7792b26addfdb800318771ee9c6
mamta0995 <mamtathapa453@gmail.com>
2023-08-03 15:33:29 -0700

    new commit

--- src/example/Test.java
+++ src/example/Test.java
@@ -3,6 +3,8 @@ package example;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Welcome world");
+		System.out.println("Welcome to world");
+		System.out.println("Welcome to the world");
 		
 	}
 	
